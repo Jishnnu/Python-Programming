e4efef7564f4838ae0d73501a162027948cb2f04
Jishnnu <jishnuanil30@gmail.com>
2023-02-15 12:21:52 +0530

    Initial Commit

--- Image_Handling/Image_handling.py
+++ Image_Handling/Image_handling.py
@@ -5,7 +5,7 @@ Write a python program to perform basic functions on an image.
 
 from PIL import Image
 
-img = Image.open('C:/Users/jishn/Documents/Programming/Python/Image_Handling/sample.jpg')
+img = Image.open('Python/Image_Handling/sample.jpg')
 
 print("\nFILENAME :", img.filename)
 print("IMAGE FORMAT :", img.format)
@@ -15,10 +15,10 @@ width, height = img.size
 print(f"Width : {width}\nHeight : {height}")
 
 img = img.resize((int(width * 2), int(height * 2)))
-img.save('C:/Users/jishn/Documents/Programming/Python/Image_Handling/resized_image.jpg')
+img.save('Python/Image_Handling/resized_image.jpg')
 
 img = img.transpose(Image.FLIP_TOP_BOTTOM)
-img.save('C:/Users/jishn/Documents/Programming/Python/Image_Handling/flipped_image.jpg')
+img.save('Python/Image_Handling/flipped_image.jpg')
 
 img = img.rotate(45)
-img.save('C:/Users/jishn/Documents/Programming/Python/Image_Handling/rotated_image.jpg')
+img.save('Python/Image_Handling/rotated_image.jpg')
